9caa996489562fa36eabbaff85285b96b01b98a3
David Flores Silva <dfloressi@unsa.edu.pe>
2021-06-29 20:22:12 -0500

    se agrego un avance de codigo del metodo delete

---
+++ ewliminacion.java
@@ -0,0 +1,33 @@
+public class ewliminacion{
+  private NodoAVL eliminacionAVL(NodoAVL nodoActual, int key) {
+    if(nodoActual == null){
+      return nodoActual;
+    }
+
+    if(key < nodoActual.key){
+      nodoActual.left = eliminacionAVL(nodoActual.left, key)
+    }else if(key > nodoActual.key){
+      nodoActual.right = eliminarAVL(nodoActual.right, key)
+    }else{
+      if((nodoActual.left == null) || (nodoActual.right == null)){
+        NodoAVL temp = null;
+        if(temp == nodoActual.left){
+          temp = nodoActual.right;
+        }else{
+          temp = nodoActual.left;
+        }
+
+        if(temp == null){
+          nodoActual = null;
+        }else{
+          nodoActual = temp;
+        }
+      }else{
+        NodoAVL temp = getNodoConValorMaximo(nodoActual.left);
+        nodoActual.key = temp.key;
+        nodoActual.left = eliminarAVL(nodoActual.left, temp.key);
+      }
+    }
+
+  }
+}
