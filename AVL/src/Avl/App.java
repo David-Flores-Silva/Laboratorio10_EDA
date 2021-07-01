09e0fa8af77f12967334da5d8ad33b2c5ae77275
David Flores Silva <dfloressi@unsa.edu.pe>
2021-06-30 19:25:32 -0500

    se agrego el resto de los ejercicios del punto 2 de la tarea en consola nuestra de manera inorden con su respectivo FE

--- AVL/src/Avl/App.java
+++ AVL/src/Avl/App.java
@@ -2,8 +2,7 @@ package Avl;
 
 public class App {
 	public static void main(String [] args) throws ItemDuplicated {
-		AVL<Integer> arbol = new AVL<Integer>();
-		
+		AVL<Integer> arbol = new AVL<Integer>();		
 		arbol.insert(44);
 		arbol.insert(70);
 		arbol.insert(30);
@@ -13,5 +12,38 @@ public class App {
 		arbol.insert(81);
 		arbol.insert(55);	
 		arbol.inOrden();
+		
+		AVL<Integer> arbol2 = new AVL<Integer>();
+		arbol2.insert(80);
+		arbol2.insert(95);
+		arbol2.insert(66);
+		arbol2.insert(70);
+		arbol2.insert(98);
+		arbol2.insert(67);
+		arbol2.inOrden();
+		
+		AVL<Integer> arbol3 = new AVL<Integer>();
+		arbol3.insert(80);
+		arbol3.insert(40);
+		arbol3.insert(60);
+		arbol3.insert(120);
+		arbol3.insert(100);
+		arbol3.insert(30);
+		arbol3.insert(130);
+		arbol3.insert(140);
+		arbol3.insert(125);
+		arbol3.insert(150);
+		arbol3.insert(20);
+		arbol3.insert(15);
+		arbol3.inOrden();
+		
+		AVL<Integer> arbol4 = new AVL<Integer>();
+		arbol4.insert(14);
+		arbol4.insert(1);
+		arbol4.insert(6);
+		arbol4.insert(17);
+		arbol4.insert(8);
+		arbol4.insert(9);
+		arbol4.inOrden();
 	}
 }
